7e0dc5bda6c2293658400671c4f78558a1dafe99
Rebekka Rendow Junget <rej003@edu.zealand.dk>
2025-05-08 13:33:17 +0200

    - Oprettet Add Method i PictursRepository - Oprettet AddTest (virker ikke)

--- QuatroCleanUpBackend/PicturesRepository.cs
+++ QuatroCleanUpBackend/PicturesRepository.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QuatroCleanUpBackend
 {
-    internal class PicturesRepository
+    public class PicturesRepository
     {
+        private readonly string _connectionString;
+
+        public PicturesRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("QuatroCleanUpDb");
+        }
+
+
+        public Pictures Add(Pictures picture)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+
+            {
+                connection.Open();
+                var command = new SqlCommand("INSERT INTO Pictures (EventId, PictureDate, Description) OUTPUT Inserted.PictureId VALUES (@EventId, @PictureDate, @Description); SELECT SCOPE_IDENTITY();", connection);
+
+                command.Parameters.AddWithValue("@EventId", picture.EventId);
+                command.Parameters.AddWithValue("@PictureDate", picture.PictureData);
+                command.Parameters.AddWithValue("@Description", picture.Description);
+
+                picture.PictureId = (int)command.ExecuteScalar();
+
+            }
+            return picture;
+        }
     }
 }
---
+++ TestPictureTest/RepositoryTest.cs
@@ -0,0 +1,36 @@
+﻿using QuatroCleanUpBackend;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+
+namespace TestPictureTest
+{
+    [TestClass]
+    public sealed class PictureRepositoryTest
+    {
+        
+
+        [TestMethod]
+        
+        public void AddTestMethod1()
+        {
+            //Arrange
+            PicturesRepository pic = new PicturesRepository();
+            
+
+            //Act
+
+            Pictures picture = pic.Add(new Pictures());
+
+            //Assert
+            Assert.AreEqual(1, picture.PictureId);
+
+        }
+    }
+}
