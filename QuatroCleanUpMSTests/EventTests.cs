3423f359a190d240837e149d7bd2f0f7ee5be879
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2025-05-08 10:09:08 +0200

    Model class til Event. Oprettet MSTests til properties, men jeg har ikke lavet dem endnu. Ikke så XP, I know xD

---
+++ QuatroCleanUpMSTests/EventTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuatroCleanUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuatroCleanUp.MSTests
+{
+    [TestClass()]
+    public class EventTests
+    {
+        [TestMethod()]
+        public void EventTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
