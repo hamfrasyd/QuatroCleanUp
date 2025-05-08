5942d09a02c122217c01820af646d181ece97442
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2025-05-08 11:52:45 +0200

    Valideringmetoder for user testet og virkerVi har lavet connectionString og den skal nu pushes til Dev branch.

---
+++ QuatroCleanUpBackend/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace QuatroCleanUpBackend
+{
+    public class UserRepository
+    {
+        private readonly string _connectionString;
+
+        public UserRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+
+
+        }
+
+       
+    }
+}
