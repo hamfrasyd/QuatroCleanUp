113bdc70038bd38affa15358917d6f604dfef6c2
Rebekka Rendow Junget <rej003@edu.zealand.dk>
2025-05-08 12:12:05 +0200

    ModelClass - Oprettet Properties - Konstuktør - ToString

---
+++ QuatroCleanUpBackend/Pictures.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuatroCleanUpBackend
+{
+    public class Pictures
+    {
+        public  int PictureId { get; set; }
+
+        public int EventId { get; set; }
+
+        public byte[]? PictureData { get; set; }
+
+        private string _description { get; set; }
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(nameof(value), "Description cannot be more than 500 characters.");
+                }
+            }
+        }
+
+        public Pictures()
+        {
+            // Default constructor
+        }
+
+
+        public string ToString()
+        {
+            return $"PictureId: {PictureId}, EventId: {EventId}, Description: {Description}";
+        }
+    }
+}
