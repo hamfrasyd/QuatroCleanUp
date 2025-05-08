75b6f99c0bc54599bce865126948a905ab0d15da
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2025-05-08 13:34:55 +0200

    Vi har oprettet MSTest klassen. Vi har oprettet create klassen. Vi har oprettet EventRepository klassen. Vi har testet og CreateEvent virker.

---
+++ EventTests/EventRepositoryTest.cs
@@ -0,0 +1,63 @@
+﻿using QuatroCleanUpBackend;
+
+namespace EventTests
+{
+    [TestClass]
+    public sealed class EventRepositoryTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            DateTime startTimeMock = new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc);
+            DateTime endTimeMock = new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc);
+            Event newEvent = new Event("Title", "Description",
+                new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc),
+                new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc),
+                true, 10, 2, 5);
+        }
+
+        [TestMethod]
+        public void CreateEventAsyncTest()
+        {
+            //Arrange
+            Event newEvent = new Event("Title", "Description", 
+                new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc),
+                new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc),
+                true,                10,                2,                5
+                );
+            EventRepository newEventRepo = new EventRepository();
+
+            //Act
+            var result = newEventRepo.CreateEventAsync(newEvent);
+
+            //Assert
+            Assert.AreEqual(result, newEvent);
+        }
+
+
+        [TestMethod]
+        public void UpdateEventTest()
+        {
+            //Arrange - we need a created event and a new event, datawise.
+            Event toBeUpdated = new Event("Title", "Description",
+            new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc),
+            new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc),
+            true, 10, 2, 5  );
+
+            Event eventUpdate = new Event("Title", "Description",
+            new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc),
+            new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc),
+            true, 10, 2, 5);
+            EventRepository newEventRepo = new EventRepository();
+
+            //Act
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ QuatroCleanUpBackend/EventRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuatroCleanUpBackend
+{
+    public class EventRepository
+    {
+        public EventRepository()
+        {
+
+        }
+
+        public Event CreateEventAsync(Event newEvent)
+        {
+            Event createEvent = newEvent;
+
+            return createEvent;
+
+        }
+
+
+    }
+}
