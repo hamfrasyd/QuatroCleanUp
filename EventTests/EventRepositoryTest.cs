064c6fd9aa2561faf5cc6adf74f00c3cb069bcf0
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2025-05-12 17:18:14 +0200

    TestUpdate should work now. Once the method has been implemented.

--- EventTests/EventRepositoryTest.cs
+++ EventTests/EventRepositoryTest.cs
@@ -1,15 +1,26 @@
-﻿using QuatroCleanUpBackend;
+﻿using Microsoft.Extensions.Logging;
+using QuatroCleanUpBackend;
 
 namespace EventTests
 {
     [TestClass]
     public sealed class EventRepositoryTest
     {
+        private Event testEvent;
+        private Event testEvent2;
+        private Event testEvent3;
+        private Event testEvent4;
+
+
         [TestInitialize]
         public void Setup()
         {
-            DateTime startTimeMock = new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc);
-            DateTime endTimeMock = new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc);
+            //DateTime startTimeMock = new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc);
+            //DateTime endTimeMock = new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc);
+            testEvent = new Event("Title", "Description",
+                new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc),
+                new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc),
+                true, 10, 2, 5);
             Event newEvent = new Event("Title", "Description",
                 new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc),
                 new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc),
@@ -20,18 +31,24 @@ namespace EventTests
         public void CreateEventAsyncTest()
         {
             //Arrange
-            Event newEvent = new Event("Title", "Description", 
-                new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc),
-                new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc),
-                true,                10,                2,                5
-                );
+
             EventRepository newEventRepo = new EventRepository();
 
             //Act
-            var result = newEventRepo.CreateEventAsync(newEvent);
+            var result = newEventRepo.CreateEventAsync(testEvent);
 
             //Assert
-            Assert.AreEqual(result, newEvent);
+            Assert.AreEqual(result, testEvent);
+        }
+
+        [TestMethod]
+        public void CreateEventAsyncCreatesEventId()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
         }
 
 
@@ -39,25 +56,54 @@ namespace EventTests
         public void UpdateEventTest()
         {
             //Arrange - we need a created event and a new event, datawise.
-            Event toBeUpdated = new Event("Title", "Description",
-            new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc),
-            new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc),
-            true, 10, 2, 5  );
+                //use testEvent - try to update that object.
 
             Event eventUpdate = new Event("Title", "Description",
             new DateTime(2025, 5, 8, 13, 05, 0, DateTimeKind.Utc),
             new DateTime(2025, 5, 8, 15, 05, 0, DateTimeKind.Utc),
             true, 10, 2, 5);
             EventRepository newEventRepo = new EventRepository();
+            int eventId = 1;
 
             //Act
+            testEvent.EventId = 1;
+            Event result = newEventRepo.UpdateEventAsync(eventId, eventUpdate);
+            result.EventId = 1;
 
-
+            //Assert
+            Assert.AreEqual(result, testEvent);
 
         }
 
+        [TestMethod]
+        public void TestEventIsDeletedFromDatabase()
+        {
+            //Arrange
 
+            //Act
 
+            //Assert
 
+        }
+
+        [TestMethod]
+        public void TestGetAll()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void TestGetById()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
     }
 }
