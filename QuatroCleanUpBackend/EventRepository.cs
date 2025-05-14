915f07e90587e50855978e762d28691e6379307c
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2025-05-14 11:06:19 +0200

    Uhm. Begyndte på repos, Kevin kigger på EventRepoTest

--- QuatroCleanUpBackend/EventRepository.cs
+++ QuatroCleanUpBackend/EventRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
 namespace QuatroCleanUpBackend
@@ -22,10 +23,24 @@ namespace QuatroCleanUpBackend
 
         }
 
+        /// <summary>
+        /// I have downloaded the Microsoft.Data.SqlClient package, because it is the
+        /// newer version. https://blog.danskingdom.com/Updating-from-System-Data-SqlClient-to-Microsoft-Data-SqlClient/
+        /// In this metho I need to create a new event object, add it to the database
+        /// and to give it an EventId which increments, with every new creation of events.
+        /// </summary>
+        /// <param name="newEvent"></param>
+        /// <returns>An Event object</returns>
         public Event CreateEventAsync(Event newEvent)
         {
             Event createEvent = newEvent;
 
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string SQLQuery = @"INSERT INTO Events ()
+                                    VALUES        ";
+            }
             return createEvent;
 
         }
